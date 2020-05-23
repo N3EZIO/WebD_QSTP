4e2de271f25dadc09b8d0e0c82c808dcb6247cb0
Nitin Arul <nitinarul68@gmail.com>
2020-05-23 13:19:56 +0530

    yeezus added

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log('hello js');
+/*
+Data types:
+undefined null boolean string symbol number and object
+
+
+
+
+
+*/
+
+var myname = 'me'; // throughout the program
+
+let num = 9; // only available in scope
+
+const pi = 3.14; // cannot be changed
+
+var lazy = 'prettier';
+
+function name() {
+	console.log('nitin');
+}
+
+name();
+
+var string = 'new stuff';
+
+console.log(string[2]);
+
+var lengt = string.length;
+
+console.log(lengt);
+
+cahr = 'A';
+
+var n = cahr.charCodeAt();
+
+console.log(typeof n);
+
+var array = [1, 2, 3, 4, 5];
+
+console.log(array);
